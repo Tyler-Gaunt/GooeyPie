c367fadbdb9ec712474e08ae301d93c64fb533c5
Tyler-Gaunt <158243239+Tyler-Gaunt@users.noreply.github.com>
2024-06-15 14:23:17 +1000

    Added in the progress bar into the checker to know load before printing the input.

--- Secret.py
+++ Secret.py
@@ -1,31 +1,41 @@
 import gooeypie as gp
+import time
 
-def display_text(event):
+def CheckPassowrd(event):
+    loading_bar.value = 0
+    result_label.text = ''
+    for steps in range(50):
+        loading_bar.value += 2
+        app.refresh()
+        time.sleep(0.02)
     entered_text = secret.text
     result_label.text = f'{entered_text}'
-
-def toggle_mask(event):
+    
+def ToggleMask(event):
     secret.toggle()
 
 app = gp.GooeyPieApp('Passowrd')
 
 question = gp.Label(app, "What's your Password?")
 
-submit_button = gp.Button(app, 'Submit', display_text)
+submit_button = gp.Button(app, 'Submit', CheckPassowrd)
 
 result_label = gp.Label(app, '')
+
+loading_bar = gp.Progressbar(app)
 
 secret = gp.Secret(app)
 secret.width = 50
 
 check = gp.Checkbox(app, 'Show passowrd')
-check.add_event_listener('change', toggle_mask)
+check.add_event_listener('change', ToggleMask)
 
-app.set_grid(5, 1)
+app.set_grid(6, 2)
 app.add(question, 1, 1)
 app.add(secret, 2, 1)
 app.add(check, 3, 1)
 app.add(submit_button, 4, 1)
-app.add(result_label, 5, 1)
+app.add(loading_bar, 5, 1, column_span=2, fill=True)
+app.add(result_label, 6, 1)
 
 app.run()
