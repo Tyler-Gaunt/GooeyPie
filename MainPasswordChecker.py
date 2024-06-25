56c650e4aade2a3616edd3f73df8e99e4874eacf
Tyler-Gaunt <158243239+Tyler-Gaunt@users.noreply.github.com>
2024-06-25 15:39:42 +1000

    This code now can tell the strength of a password by checking the length and characters used. I also decided to call it PassWizard.

--- MainPasswordChecker.py
+++ MainPasswordChecker.py
@@ -1,34 +1,49 @@
 import gooeypie as gp
 import time
+import re
 
 def CheckPassowrd(event):
     loading_bar.value = 0
-    result_label.text = ''
+    result_label.text = ""
     for steps in range(50):
         loading_bar.value += 2
         app.refresh()
         time.sleep(0.02)
-    entered_text = secret.text
-    result_label.text = f'{entered_text}'
     
+    entered_text = secret.text
+    if check_password_strength(entered_text) == "Strong":
+        result_label.text = "Password Lvl: Strong"
+    elif check_password_strength(entered_text) == "Moderate":
+        result_label.text = "Password Lvl: Moderate, Please make sure your password contains at least; \n13 characters, one upper and lower case letter, one number and \none special character!"
+    else:
+        result_label.text = "Password Lvl: Weak, Please make sure your password contains at least; \n13 characters, one upper and lower case letter, one number and \none special character!"
+
 def ToggleMask(event):
     secret.toggle()
 
-app = gp.GooeyPieApp('Passowrd')
+def check_password_strength(password):
+    if len(password) > 13 and re.search(r'[A-Z]', password) and re.search(r'[a-z]', password) and re.search(r'\d', password) and re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+        return "Strong"
+    elif len(password) > 13 and (re.search(r'[A-Z]', password) or re.search(r'[a-z]', password)) and (re.search(r'\d', password) or re.search(r'[!@#$%^&*(),.?":{}|<>]', password)):
+        return "Moderate"
+    elif len(password) < 13:
+        return "Weak"
+
+app = gp.GooeyPieApp("PassWizard")
 
 question = gp.Label(app, "What's your Password?")
 
-submit_button = gp.Button(app, 'Submit', CheckPassowrd)
+submit_button = gp.Button(app, "Submit", CheckPassowrd)
 
-result_label = gp.Label(app, '')
+result_label = gp.Label(app, "")
 
 loading_bar = gp.Progressbar(app)
 
 secret = gp.Secret(app)
 secret.width = 50
 
-check = gp.Checkbox(app, 'Show passowrd')
-check.add_event_listener('change', ToggleMask)
+check = gp.Checkbox(app, "Show passowrd")
+check.add_event_listener("change", ToggleMask)
 
 app.set_grid(6, 2)
 app.add(question, 1, 1)
@@ -39,3 +54,4 @@ app.add(loading_bar, 5, 1, column_span=2, fill=True)
 app.add(result_label, 6, 1)
 
 app.run()
+
